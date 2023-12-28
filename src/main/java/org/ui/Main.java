a98dbf2c2039c8c5f3f273b5f24f50f4b2835ea4
Uboatwaffe <mpiatek1512@gmail.com>
2023-12-28 16:36:29 +0100

    Backbone
    Signed-off-by: Uboatwaffe <mpiatek1512@gmail.com>

---
+++ src/main/java/org/ui/Main.java
@@ -0,0 +1,17 @@
+package java.org.ui;
+
+/**
+ * Created on 28.12.2023
+ *
+ * @author Maciek
+ * @version 0.1
+ */
+public class Main {
+
+    /**
+     * Constructor
+     */
+    Main() {
+
+    }
+}
